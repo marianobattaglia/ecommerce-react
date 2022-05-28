cf05b11758ced4bd9b833ceb70f4f475804e5f12
Mariano Martin Battaglia <battaglia1902@gmail.com>
2022-05-28 19:39:02 -0300

    Se agrega contador al proyecto para luego realizar funcionalidad dentro de las cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import Counter from './components/Counter/Counter.js';
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Button from './components/Button/Button'
 
 function App() {
   
+  {/*
+  const [show, setShow] = useState(true)
+
+  const handleAdd = () => {
+    console.log('se ejecuto')
+  }
+  */}
+
   return (
     <div className="App">
       <NavBar className="navBar"/>
       <ItemListContainer saludo={"Hola coders"}/>
 
-      <Button label="asd"/>
-
+      {/* show && <Counter initial={10} stock={50} onAdd={handleAdd}/> */}
     </div>
   );
 }
